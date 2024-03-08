1cb185e402cd31f54c4301a8d92311a2c03b6d9a
AGENTSJ <abhijithsj4@gmail.com>
2024-03-08 13:15:44 +0530

    test

--- front/js/assets.js
+++ front/js/assets.js
@@ -2,32 +2,32 @@
 let obstImagePaths = [
     
     {
-        path:"../assets/obstacles/cactus1.png",
+        path:"./assets/obstacles/cactus1.png",
         height:35,
         width:25
     },
     {
-        path:"../assets/obstacles/cactus2.png",
+        path:"./assets/obstacles/cactus2.png",
         height:35,
         width:30
     },
     {
-        path:"../assets/obstacles/cactus3.png",
+        path:"./assets/obstacles/cactus3.png",
         height:35,
         width:45
     },
     {
-        path:"../assets/obstacles/big-cactus1.png",
+        path:"./assets/obstacles/big-cactus1.png",
         height:50,
         width:30
     },
     {
-        path:"../assets/obstacles/big-cactus2.png",
+        path:"./assets/obstacles/big-cactus2.png",
         height:50,
         width:60
     },
     {
-        path:"../assets/obstacles/big-cactus3.png",
+        path:"./assets/obstacles/big-cactus3.png",
         height:50,
         width:90
     }
@@ -35,7 +35,7 @@ let obstImagePaths = [
 
 let playerPath = [
     {
-        path:"../assets/player/dino.png",
+        path:"./assets/player/dino.png",
         height:50,
         width:50
     }
